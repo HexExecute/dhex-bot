ad69dac7283ecea542cd84bc324a068dceaefef1
Hex <hex.execute@gmail.com>
2022-05-28 16:41:06 -0400

    Added kick command, started attempting to fix dashboard.

---
+++ src/Commands/kick.ts
@@ -0,0 +1,51 @@
+import { GuildMember, MessageEmbed } from 'discord.js'
+import { Command } from '../Structures/Command'
+
+export default new Command({
+  name: 'kick',
+  description: 'A command to kick members.',
+  usage: 'kick !(member) !(reason)',
+  permissions: ['KICK_MEMBERS'],
+  options: [
+    {
+      name: 'member',
+      description: 'member to kick',
+      type: 'USER',
+      required: true,
+    },
+    {
+      name: 'reason',
+      description: 'reason for kick',
+      type: 'STRING',
+      required: true,
+    },
+  ],
+  execute: async ({ interaction, args, client, author }) => {
+    const target: GuildMember = await client.guild.members.fetch(args.member)
+
+    if (!target.kickable)
+      return interaction.editReply(`Sorry, but you can't kick that person!`)
+
+    target.kick(args.reason).then(() =>
+      interaction.editReply({
+        embeds: [
+          new MessageEmbed()
+            .setTitle('Kick')
+            .setDescription(`<@${author.id}> has kicked <@${args.member}>`)
+            .setThumbnail(target.user.displayAvatarURL())
+            .addField('Reason', args.reason)
+            .setAuthor({
+              name: target.user.tag,
+              iconURL: target.user.displayAvatarURL(),
+            })
+            .setFooter({
+              text: author.user.tag,
+              iconURL: author.displayAvatarURL(),
+            })
+            .setTimestamp()
+            .setColor('#C03D2F'),
+        ],
+      })
+    )
+  },
+})
